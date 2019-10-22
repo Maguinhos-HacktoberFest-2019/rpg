05e13f996a053907bc184c7cf9911c630db2ca8a
Gary M. Silva <garymsilva@outlook.com>
2019-10-22 16:48:36 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,71 @@
 from phases.start import start 
 
-phases = [start]
+g_phases = [start]
 
-def main():
-    pass
+class Player():
+    name = "Youngling"
+    isAlive = True
+    
+    def __init__(self, settings):
+        self.settings = settings
+        self.name = "Youngling" #TODO: Pegar nome pelo prompt
+    #
 #
 
-if __name__ == __main__:
+class Settings():
+    hardcore = False
+    
+    def __init__(self):
+        config()
+    #
+    
+    def config(self):
+        #TODO: coletar configurações do usuário
+        pass
+    #
+#
+
+def rand(min, max):
+    #TODO: Implementar o random
+    return
+#
+
+def play(id):
+    phase = g_phases[id]
+    result = phase()
+    
+    if result:
+        g_phases.pop(id)
+    #
+    
+    return result
+#
+
+def runGame(player):    
+    # Engine
+    while len(g_phases) > 0:
+        phaseId = rand(0, len(g_phases))
+        phaseResult = play(phaseId)
+        
+        if player.settings.hardcore && not phaseResult:
+            print("Você perdeu! Inicie o jogo novamente.")
+            break
+        #
+        
+        while not phaseResult:
+            print("Tente novamente!")
+            phaseResult = play(phaseId)
+        #
+    #
+#
+
+def main():
+    # TODO: Adicionar mensagem de boas vindas!
+    settings = Settings()
+    player = Player(settings)
+    runGame(player)
+#
+
+if __name__ == "__main__":
     main()
 #
