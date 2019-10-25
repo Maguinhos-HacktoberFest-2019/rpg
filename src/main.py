70e3d7a6283b41cbc49b402cda4c8d717a9976ad
rafaelapessin <rafaela_pessin@outlook.com>
2019-10-25 17:26:11 -0300

    importando a fase criada

--- src/main.py
+++ src/main.py
@@ -3,8 +3,9 @@ from phases.start import start
 from phases.hangman import hangman
 from phases.dilema import dilema
 from phases.velha import velha
+from phases.caracoroa import caraCoroa
 
-g_phases = [start, dilema, velha, hangman]
+g_phases = [start, dilema, velha, hangman, caraCoroa]
 
 class Settings():
     hardcore = False
